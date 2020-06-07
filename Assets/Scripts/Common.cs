19b09c6773758cc8968d59f2b91d2ce84b11d531
Péter Kovács <peti1106@gmail.com>
2020-06-08 00:21:48 +0200

    Load resources from .iwd archives

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -1,33 +1,130 @@
-﻿using System.IO;
+﻿using UnityEngine;
 
-namespace Potion
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+public static class Utils
 {
-    public static class Utils
+    public static string CoD2Path = "Assets\\Resources\\";
+
+    public static string CoD2IwdPath = Application.streamingAssetsPath;
+
+    public static Dictionary<string, byte[]> maps = new Dictionary<string, byte[]>();
+    public static Dictionary<string, byte[]> materials = new Dictionary<string, byte[]>();
+    public static Dictionary<string, byte[]> images = new Dictionary<string, byte[]>();
+    public static Dictionary<string, byte[]> xmodel = new Dictionary<string, byte[]>();
+    public static Dictionary<string, byte[]> xmodelparts = new Dictionary<string, byte[]>();
+    public static Dictionary<string, byte[]> xmodelsurfs = new Dictionary<string, byte[]>();
+
+    public static string ReadStringTerminated(this BinaryReader br, byte terminatingChar = 0x00)
     {
-        public static string CoD2Path = "Assets\\Resources\\";
+        char[] rawName = new char[64];
 
-        public static string ReadStringTerminated( this BinaryReader br, byte terminatingChar = 0x00 ) 
+        int i = 0;
+        for (i = 0; i < 64; i++)
         {
-            char[] rawName = new char[64];
+            if (br.PeekChar() == terminatingChar)
+                break;
 
-            int i = 0;
-            for( i = 0; i < 64; i++ ) {
-                if( br.PeekChar() == terminatingChar )
-                    break;
-
-                rawName[i] = br.ReadChar();
-            }
-
-            return new string( rawName ).Replace( "\0", string.Empty ).Trim();
+            rawName[i] = br.ReadChar();
         }
 
-        public static string ReadStringLength( this BinaryReader br, uint length ) 
+        return new string(rawName).Replace("\0", string.Empty).Trim();
+    }
+
+    public static string ReadStringLength(this BinaryReader br, uint length)
+    {
+        char[] rawName = new char[length];
+
+        rawName = br.ReadChars((int)length);
+
+        return new string(rawName).Replace("\0", string.Empty).Trim();
+    }
+
+    public static void ReadZipContentsToDictionaries()
+    {
+        string fileCounterFormat = "00";
+        List<string> iwdNames = new List<string>();
+
+        for (int numbersOfIwds = 0; numbersOfIwds <= 15; numbersOfIwds++)
         {
-            char[] rawName = new char[length];
+            iwdNames.Add(Path.Combine(CoD2IwdPath, "iw_" + numbersOfIwds.ToString(fileCounterFormat) + ".iwd"));
+        }
 
-            rawName = br.ReadChars( (int) length );
+        foreach (string iwdName in iwdNames)
+        {
+            Debug.Log(iwdName);
+            using (ZipArchive archive = ZipFile.OpenRead(iwdName))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (!string.IsNullOrEmpty(entry.Name))
+                    {
+                        //Debug.Log(entry.FullName);
+                        AddEntryToDictionaryByType(entry);
+                    }
+                }
+            }
+        }
 
-            return new string( rawName ).Replace( "\0", string.Empty ).Trim();
+        /*foreach (KeyValuePair<string, ZipArchiveEntry> entry in maps)
+        {
+            Debug.Log(entry.Key);
+        }*/
+    }
+
+    private static string GetFolderName(string entryFullName)
+    {
+        int found = entryFullName.IndexOf("/");
+        return (found > 0) ? entryFullName.Substring(0, found) : "";
+    }
+
+    private static void AddEntryToDictionaryByType(ZipArchiveEntry entry)
+    {
+        string folderName = GetFolderName(entry.FullName);
+
+        switch (folderName)
+        {
+            case "maps":
+                CreateNewOrUpdateExisting(maps, entry.Name, ReadStream(entry.Open()));
+                break;
+            case "materials":
+                CreateNewOrUpdateExisting(materials, entry.Name, ReadStream(entry.Open()));
+                break;
+            case "images":
+                CreateNewOrUpdateExisting(images, entry.Name, ReadStream(entry.Open()));
+                break;
+            case "xmodel":
+                CreateNewOrUpdateExisting(xmodel, entry.Name, ReadStream(entry.Open()));
+                break;
+            case "xmodelparts":
+                CreateNewOrUpdateExisting(xmodelparts, entry.Name, ReadStream(entry.Open()));
+                break;
+            case "xmodelsurfs":
+                CreateNewOrUpdateExisting(xmodelsurfs, entry.Name, ReadStream(entry.Open()));
+                break;
+        }
+    }
+
+    public static void CreateNewOrUpdateExisting<TKey, TValue>(this IDictionary<TKey, TValue> map, TKey key, TValue value)
+    {
+        if (map.ContainsKey(key))
+        {
+            map[key] = value;
+        }
+        else
+        {
+            map.Add(key, value);
+        }
+    }
+
+    private static byte[] ReadStream(Stream stream)
+    {
+        using (var ms = new MemoryStream())
+        {
+            stream.CopyTo(ms);
+            return ms.ToArray();
         }
     }
 }
--- Assets/Scripts/D3DBSP.cs
+++ Assets/Scripts/D3DBSP.cs
@@ -1,112 +1,123 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 using System;
 using System.IO;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Potion {
+public struct Lump
+{
+    public string name;
 
-    public struct Lump {
-        public string name;
+    public uint offset;
+    public uint length;
+};
 
-        public uint offset;
-        public uint length;
-    };
+public struct TriangleSoup
+{
+    public ushort materialID;
 
-    public struct TriangleSoup {
-        public ushort materialID;
+    public ushort drawOrder;
 
-        public ushort drawOrder;
+    public uint vertexOffset;
+    public ushort vertexLength;
 
-        public uint vertexOffset;
-        public ushort vertexLength;
+    public uint triangleOffset;
+    public ushort triangleLength;
+};
 
-        public uint triangleOffset;
-        public ushort triangleLength;
-    };
-
-    public class Triangle {
-        public Triangle() {
-            indices = new ushort[ 3];
-        }
-
-        public ushort[] indices;
+public class Triangle
+{
+    public Triangle()
+    {
+        indices = new ushort[3];
     }
 
-    public class Vertex {
-        public Vertex() {
-            position = new float[3];
-            normal = new float[3];
-            rgba = new byte[4];
+    public ushort[] indices;
+}
 
-            uv = new float[2];
-            st = new float[2];
+public class Vertex
+{
+    public Vertex()
+    {
+        position = new float[3];
+        normal = new float[3];
+        rgba = new byte[4];
 
-            unknown = new float[6];
-        }
+        uv = new float[2];
+        st = new float[2];
 
-        public Vector3 PositionToVector3() {
-            Vector3 pos = new Vector3( 
-                position[0],
-                position[1],
-                position[2]
-            );
-
-            return pos;
-        }
-
-        public Vector2 UVToVector2() {
-            Vector2 _uv = new Vector2(
-                uv[0],
-                uv[1]
-            );
-
-            return _uv;
-        }
-
-        public float[] position;
-        public float[] normal;
-        public byte[] rgba;
-
-        public float[] uv;
-        public float[] st;
-
-        public float[] unknown;
+        unknown = new float[6];
     }
 
-    public class MapMaterial {
-        public string name;
-        public long flags;
+    public Vector3 PositionToVector3()
+    {
+        Vector3 pos = new Vector3(
+            position[0],
+            position[1],
+            position[2]
+        );
+
+        return pos;
     }
 
-    public class D3DBSP : MonoBehaviour {
-        public GameObject root;
+    public Vector2 UVToVector2()
+    {
+        Vector2 _uv = new Vector2(
+            uv[0],
+            uv[1]
+        );
 
-        public Material defaultMaterial;
+        return _uv;
+    }
 
-        Dictionary<int, string> lumpNames;
+    public float[] position;
+    public float[] normal;
+    public byte[] rgba;
 
-        List<Lump> lumps;
-        List<TriangleSoup> triangleSoups;
+    public float[] uv;
+    public float[] st;
 
-        List<Vertex> vertices;
-        List<Triangle> triangles;
+    public float[] unknown;
+}
 
-        List<MapMaterial> materials;
+public class MapMaterial
+{
+    public string name;
+    public long flags;
+}
 
-        FileStream fs;
-        BinaryReader br;
+public class D3DBSP : MonoBehaviour
+{
+    public GameObject root;
 
-        MaterialCreator materialCreator;
+    public Material defaultMaterial;
 
-        void Start() {
-            Load();
-        }
+    Dictionary<int, string> lumpNames;
 
-        void Load() {
-            lumpNames = new Dictionary<int, string>() {
+    List<Lump> lumps;
+    List<TriangleSoup> triangleSoups;
+
+    List<Vertex> vertices;
+    List<Triangle> triangles;
+
+    List<MapMaterial> materials;
+
+    MemoryStream fs;
+    BinaryReader br;
+
+    MaterialCreator materialCreator;
+
+    void Start()
+    {
+        Utils.ReadZipContentsToDictionaries();
+        Load();
+    }
+
+    void Load()
+    {
+        lumpNames = new Dictionary<int, string>() {
                 { 0, "Materials" },
                 { 1, "Lightmaps" },
                 { 2, "Light Grid Hash" },
@@ -142,229 +153,253 @@ namespace Potion {
                 { 38, "Paths" },
             };
 
-            lumps = new List<Lump>();
-            materials = new List<MapMaterial>();
-            vertices = new List<Vertex>();
-            triangles = new List<Triangle>();
-            triangleSoups = new List<TriangleSoup>();
+        lumps = new List<Lump>();
+        materials = new List<MapMaterial>();
+        vertices = new List<Vertex>();
+        triangles = new List<Triangle>();
+        triangleSoups = new List<TriangleSoup>();
 
-            materialCreator = new MaterialCreator(defaultMaterial);
+        materialCreator = new MaterialCreator(defaultMaterial);
 
-            using (fs = new FileStream(Utils.CoD2Path + "main\\maps\\mp\\mp_carentan.d3dbsp", FileMode.Open, FileAccess.Read)) {
-                using (br = new BinaryReader(fs, new ASCIIEncoding())) {
-                    if (fs.CanRead)
-                        StartReading();
-                }
-            }
-        }
-
-        private void StartReading() {
-            string ident = GetHeaderIdentifier();
-
-            string mapNameWithExt = fs.Name.Substring(fs.Name.LastIndexOf('/') + 1);
-            string mapName = mapNameWithExt.Substring(0, mapNameWithExt.IndexOf('.'));
-
-            Debug.Log("File format " + ident + " has been detected on " + mapName);
-
-            if (ident != "IBSP4")
-                return;
-
-            FillLumpList();
-        }
-
-        string GetHeaderIdentifier()
+        using (fs = new MemoryStream(Utils.maps["mp_carentan.d3dbsp"]))
         {
-            byte[] chunk;
-            chunk = br.ReadBytes(5);
-
-            StringBuilder ident = new StringBuilder();
-
-            for (int i = 0; i < 4; i++) {
-                ident.Append((char) chunk[i]);
+            using (br = new BinaryReader(fs, new ASCIIEncoding()))
+            {
+                if (fs.CanRead)
+                    StartReading();
             }
-            ident.Append((int) chunk[4]);
+        }
+    }
 
-            return ident.ToString();
+    private void StartReading()
+    {
+        string ident = GetHeaderIdentifier();
+
+        //string mapNameWithExt = fs.Name.Substring(fs.Name.LastIndexOf('/') + 1);
+        //string mapName = mapNameWithExt.Substring(0, mapNameWithExt.IndexOf('.'));
+        string mapName = "mp_carentan.d3dbsp";
+
+        Debug.Log("File format " + ident + " has been detected on " + mapName);
+
+        if (ident != "IBSP4")
+            return;
+
+        FillLumpList();
+    }
+
+    string GetHeaderIdentifier()
+    {
+        byte[] chunk;
+        chunk = br.ReadBytes(5);
+
+        StringBuilder ident = new StringBuilder();
+
+        for (int i = 0; i < 4; i++)
+        {
+            ident.Append((char)chunk[i]);
+        }
+        ident.Append((int)chunk[4]);
+
+        return ident.ToString();
+    }
+
+    private void FillLumpList()
+    {
+        br.BaseStream.Seek(8, SeekOrigin.Begin);
+
+        for (int i = 0; i < 39; i++)
+        {
+            string lumpName;
+
+            if (!lumpNames.ContainsKey(i))
+            {
+                lumpName = "UNKNOWN";
+            }
+            else
+            {
+                lumpName = lumpNames[i];
+            }
+
+            Lump l = new Lump();
+
+            l.name = lumpName;
+            l.length = br.ReadUInt32();
+            l.offset = br.ReadUInt32();
+
+            lumps.Insert(i, l);
+
+            Debug.Log("Lump[" + l.name + "] Length: " + l.length + " bytes | Offset: " + l.offset + " bytes");
         }
 
-        private void FillLumpList() {
-            br.BaseStream.Seek(8, SeekOrigin.Begin);
+        CreateMeshMagic();
+    }
 
-            for (int i = 0; i < 39; i++) {
-                string lumpName;
+    private void FillMaterialList()
+    {
+        br.BaseStream.Seek(lumps[0].offset, SeekOrigin.Begin);
 
-                if (!lumpNames.ContainsKey(i)) {
-                    lumpName = "UNKNOWN";
-                } else {
-                    lumpName = lumpNames[i];
+        for (int i = 0; i < lumps[0].length; i++)
+        {
+            MapMaterial m = new MapMaterial();
+
+            m.name = Encoding.ASCII.GetString(br.ReadBytes(64)).Replace("\0", string.Empty).Trim();
+            m.flags = br.ReadInt64();
+
+            materials.Add(m);
+        }
+    }
+
+    void FillSoupsList()
+    {
+        br.BaseStream.Seek(lumps[7].offset, SeekOrigin.Begin);
+
+        for (int i = 0; i < lumps[7].length / 16; i++)
+        {
+            TriangleSoup t = new TriangleSoup();
+
+            t.materialID = br.ReadUInt16();
+            t.drawOrder = br.ReadUInt16();
+
+            t.vertexOffset = br.ReadUInt32();
+            t.vertexLength = br.ReadUInt16();
+
+            t.triangleLength = br.ReadUInt16();
+            t.triangleOffset = br.ReadUInt32();
+
+            triangleSoups.Add(t);
+        }
+    }
+
+    void FillVerticesList()
+    {
+        br.BaseStream.Seek(lumps[8].offset, SeekOrigin.Begin);
+
+        for (int i = 0; i < lumps[8].length / 68; i++)
+        {
+            Vertex v = new Vertex();
+
+            v.position[0] = br.ReadSingle();
+            v.position[2] = br.ReadSingle(); // switch Y and Z, different engine
+            v.position[1] = br.ReadSingle();
+
+            v.normal[0] = br.ReadSingle();
+            v.normal[1] = br.ReadSingle();
+            v.normal[2] = br.ReadSingle();
+
+            v.rgba[0] = br.ReadByte();
+            v.rgba[1] = br.ReadByte();
+            v.rgba[2] = br.ReadByte();
+            v.rgba[3] = br.ReadByte();
+
+            v.uv[0] = br.ReadSingle();
+            v.uv[1] = br.ReadSingle();
+
+            v.st[0] = br.ReadSingle();
+            v.st[1] = br.ReadSingle();
+
+            // Unknown.. skip. Texture rotation?
+            br.BaseStream.Seek(24, SeekOrigin.Current);
+
+            vertices.Add(v);
+        }
+    }
+
+    void FillTrianglesList()
+    {
+        br.BaseStream.Seek(lumps[9].offset, SeekOrigin.Begin);
+
+        for (int i = 0; i < lumps[9].length / 6; i++)
+        {
+            Triangle t = new Triangle();
+
+            t.indices[0] = br.ReadUInt16();
+            t.indices[1] = br.ReadUInt16();
+            t.indices[2] = br.ReadUInt16();
+
+            triangles.Add(t);
+        }
+    }
+
+    void CreateMeshMagic()
+    {
+
+        FillSoupsList();
+        FillVerticesList();
+        FillTrianglesList();
+        FillMaterialList();
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        List<int> triangleIndices = new List<int>();
+
+        // 1 soup per material/mesh
+        // Each mesh has a triangle_length and triangle_offset
+        // Triangle_length is how many triangles the mesh uses
+        // Triangle_offset is the offset into the triangle array
+
+        // Each soup also defines a vertex_offset
+        // First you look up the current triangle
+        // Then you use the index pointed to by the triangle, plus the vertex_offset, to find the correct vertex
+
+        for (int i = 0; i < this.triangleSoups.Count; i++)
+        {
+            TriangleSoup currentSoup = this.triangleSoups[i];
+
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.transform.parent = root.transform;
+
+            Mesh m = new Mesh();
+            go.GetComponent<MeshFilter>().mesh = m;
+
+            int tri_count = (int)currentSoup.triangleLength / 3;
+
+            for (int j = 0; j < tri_count; j++)
+            {
+                Triangle tri = this.triangles[(int)currentSoup.triangleOffset / 3 + j];
+
+                for (int vert_loop = 0; vert_loop < 3; vert_loop++)
+                {
+                    int offset = (int)tri.indices[vert_loop];
+
+                    Vector3 pos = this.vertices[(int)currentSoup.vertexOffset + offset].PositionToVector3();
+                    Vector2 uv = this.vertices[(int)currentSoup.vertexOffset + offset].UVToVector2();
+
+                    triangleIndices.Add(vertices.Count);
+                    vertices.Add(pos);
+                    uvs.Add(uv);
                 }
-
-                Lump l = new Lump();
-
-                l.name = lumpName;
-                l.length = br.ReadUInt32();
-                l.offset = br.ReadUInt32();
-
-                lumps.Insert(i, l);
-
-                Debug.Log("Lump[" + l.name + "] Length: " + l.length + " bytes | Offset: " + l.offset + " bytes");
             }
 
-            CreateMeshMagic();
-        }
+            if (vertices.Count > 0)
+            {
+                // Load required material here
+                Material newMat = materialCreator.CreateMaterial(materials[currentSoup.materialID].name);
 
-        private void FillMaterialList() {
-            br.BaseStream.Seek(lumps[0].offset, SeekOrigin.Begin);
+                // noDraw
+                if ((materials[currentSoup.materialID].flags & 0x0000000100000080) == 0)
+                    go.SetActive(false);
 
-            for (int i = 0; i < lumps[0].length; i++) {
-                MapMaterial m = new MapMaterial();
+                go.GetComponent<Renderer>().material = newMat;
 
-                m.name = Encoding.ASCII.GetString(br.ReadBytes(64)).Replace("\0", string.Empty).Trim();
-                m.flags = br.ReadInt64();
+                m.vertices = vertices.ToArray();
+                m.triangles = triangleIndices.ToArray();
+                m.uv = uvs.ToArray();
 
-                materials.Add(m);
+                m.RecalculateNormals();
+
+                vertices.Clear();
+                triangleIndices.Clear();
+                uvs.Clear();
             }
-        }
-
-        void FillSoupsList() {
-            br.BaseStream.Seek(lumps[7].offset, SeekOrigin.Begin);
-
-            for (int i = 0; i < lumps[7].length / 16; i++) {
-                TriangleSoup t = new TriangleSoup();
-
-                t.materialID = br.ReadUInt16();
-                t.drawOrder = br.ReadUInt16();
-
-                t.vertexOffset = br.ReadUInt32();
-                t.vertexLength = br.ReadUInt16();
-
-                t.triangleLength = br.ReadUInt16();
-                t.triangleOffset = br.ReadUInt32();
-
-                triangleSoups.Add(t);
+            else
+            {
+                Destroy(go);
             }
         }
 
-        void FillVerticesList() {
-            br.BaseStream.Seek(lumps[8].offset, SeekOrigin.Begin);
+        root.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
-            for (int i = 0; i < lumps[8].length / 68; i++) {
-                Vertex v = new Vertex();
-
-                v.position[0] = br.ReadSingle();
-                v.position[2] = br.ReadSingle(); // switch Y and Z, different engine
-                v.position[1] = br.ReadSingle();
-
-                v.normal[0] = br.ReadSingle();
-                v.normal[1] = br.ReadSingle();
-                v.normal[2] = br.ReadSingle();
-
-                v.rgba[0] = br.ReadByte();
-                v.rgba[1] = br.ReadByte();
-                v.rgba[2] = br.ReadByte();
-                v.rgba[3] = br.ReadByte();
-
-                v.uv[0] = br.ReadSingle();
-                v.uv[1] = br.ReadSingle();
-
-                v.st[0] = br.ReadSingle();
-                v.st[1] = br.ReadSingle();
-
-                // Unknown.. skip. Texture rotation?
-                br.BaseStream.Seek(24, SeekOrigin.Current);
-
-                vertices.Add(v);
-            }
-        }
-
-        void FillTrianglesList() {
-            br.BaseStream.Seek(lumps[9].offset, SeekOrigin.Begin);
-
-            for (int i = 0; i < lumps[9].length / 6; i++) {
-                Triangle t = new Triangle();
-
-                t.indices[0] = br.ReadUInt16();
-                t.indices[1] = br.ReadUInt16();
-                t.indices[2] = br.ReadUInt16();
-
-                triangles.Add(t);
-            }
-        }
-
-        void CreateMeshMagic() {
-
-            FillSoupsList();
-            FillVerticesList();
-            FillTrianglesList();
-            FillMaterialList();
-
-            List<Vector3> vertices = new List<Vector3>();
-            List<Vector2> uvs = new List<Vector2>();
-
-            List<int> triangleIndices = new List<int>();
-
-            // 1 soup per material/mesh
-            // Each mesh has a triangle_length and triangle_offset
-            // Triangle_length is how many triangles the mesh uses
-            // Triangle_offset is the offset into the triangle array
-
-            // Each soup also defines a vertex_offset
-            // First you look up the current triangle
-            // Then you use the index pointed to by the triangle, plus the vertex_offset, to find the correct vertex
-
-            for (int i = 0; i < this.triangleSoups.Count; i++) {
-                TriangleSoup currentSoup = this.triangleSoups[i];
-
-                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                go.transform.parent = root.transform;
-
-                Mesh m = new Mesh();
-                go.GetComponent<MeshFilter>().mesh = m;
-
-                int tri_count = (int) currentSoup.triangleLength / 3;
-                
-                for (int j = 0; j < tri_count; j++) {
-                    Triangle tri = this.triangles[(int) currentSoup.triangleOffset / 3 + j];
-
-                    for (int vert_loop = 0; vert_loop < 3; vert_loop++) {
-                        int offset = (int) tri.indices[vert_loop];
-
-                        Vector3 pos = this.vertices[(int) currentSoup.vertexOffset + offset].PositionToVector3();
-                        Vector2 uv = this.vertices[(int) currentSoup.vertexOffset + offset].UVToVector2();
-
-                        triangleIndices.Add(vertices.Count);
-                        vertices.Add(pos);
-                        uvs.Add(uv);
-                    }
-                }
-
-                if (vertices.Count > 0) {
-                    // Load required material here
-                    Material newMat = materialCreator.CreateMaterial(materials[currentSoup.materialID].name);
-
-                    // noDraw
-                    if ((materials[currentSoup.materialID].flags & 0x0000000100000080) == 0)
-                        go.SetActive(false);
-
-                    go.GetComponent<Renderer>().material = newMat;
-
-                    m.vertices = vertices.ToArray();
-                    m.triangles = triangleIndices.ToArray();
-                    m.uv = uvs.ToArray();
-                    
-                    m.RecalculateNormals();
-
-                    vertices.Clear();
-                    triangleIndices.Clear();
-                    uvs.Clear();
-                } else {
-                    Destroy(go);
-                }
-            }
-
-            root.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
